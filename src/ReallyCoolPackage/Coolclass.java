7ad6098559ed169d05a4ce545abca498158470df
league <league@imac-38.attlocal.net>
2023-03-20 18:33:34 -0700

    Instead of calculation finaly in the lines class, do it in coolclass, the current issue is that the lines aren't connected

--- src/ReallyCoolPackage/Coolclass.java
+++ src/ReallyCoolPackage/Coolclass.java
@@ -1,7 +1,7 @@
 package ReallyCoolPackage;
 
 import java.util.ArrayList;
-
+import java.util.Random;
 
 import processing.core.PApplet;
 
@@ -10,21 +10,25 @@ public class Coolclass extends PApplet {
 	static final int WIDTH = 1250;
 	static final int HEIGHT = 750;
 
+	
+	Random ccr = new Random();
+	
+	int changedir=0;
 		//speed of the lines, should be chosen by the user, or oculd be entered through
-		int speed = 15;
+		int speed = 4;
 		//size of the lines, same stuff after the comma as before
-		int thin = 20;
+		int thin = 50;
 		//The height
-		int tall = 450;
+		int tall = 100;
 		//(should be a rage)how long things should be chained togegther before switching directicten (as in up and down), same stuff
-		int chain =3; //set to an amount of lines for now
+		int chains =2; 
+		int chainb = 4;
+		int chainf;
 		//finds the last line so it can find out when to add a new line
 		int large;
-		//used to find large
-		int small=0;
-		//used when I was lowering the lines to see if they were being made
-		int debugy=10;
-		
+		//(should be a range)the max distance the lines can be apart
+		int ranges = 35;
+		int rangeb = 35;
 		
 		
 		ArrayList<lines> lines = new ArrayList<lines>();
@@ -34,6 +38,11 @@ public class Coolclass extends PApplet {
 		//this is where things get set to stuff
 		size(WIDTH,HEIGHT);
 		lines.add(new lines(this));
+	}
+	public int chaincheck() {
+		int chain = chainf=ccr.nextInt(chainb)+chains;
+		System.out.println("chain " + chainf);
+		return chain;
 	}
 		//new Segment(hx, hy, this
 	@Override
@@ -53,7 +62,7 @@ public class Coolclass extends PApplet {
 		
 		void movelines() {
 			for (int i = 0; i < lines.size(); i++) {
-				lines.get(i).x--;
+				lines.get(i).x-=speed;
 			}
 			
 		}
@@ -64,7 +73,7 @@ public class Coolclass extends PApplet {
 			}
 			if(lines.get(large).x<(WIDTH)-thin) {
 				lines.add(new lines(this));
-				debugy++;
+			
 			}
 			
 			
@@ -75,6 +84,7 @@ public class Coolclass extends PApplet {
 	
 public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 	PApplet.main(Coolclass.class.getName());
 	
 }
--- src/ReallyCoolPackage/lines.java
+++ src/ReallyCoolPackage/lines.java
@@ -1,18 +1,90 @@
 package ReallyCoolPackage;
 
+import java.util.Random;
+
 import processing.core.PApplet;
 
 public class lines {
 	int x = 1250;
-	
-	Coolclass cc = new Coolclass();
-	int y=cc.debugy;
-	 PApplet pro;
-	public lines(PApplet pro){
-	 this.pro = pro;
+	Integer y;
+	int lasty;
+	int finaly;
+	Random r = new Random();
+	Coolclass pro;
+	Boolean isup =true;
+	public lines(Coolclass pro) {
+		
+		
+		
+		
+		
+		this.pro = pro;
+		y = pro.lines.size();
+		if(pro.lines.isEmpty()==true) {
+			lasty=pro.height/2;
+		}
+		
+		
+		
+		else {
+			
+			
+			
+			//makes the chaining stuff work
+			lasty=pro.lines.get(pro.lines.size()-2).y;
+		}
+		if(pro.changedir!=0) {
+			 isup=r.nextBoolean();
+		}
+		else {
+		pro.changedir=	pro.chaincheck();
+		}
+		
+		//where I make the distance
+			int ry = r.nextInt(pro.rangeb) + pro.ranges;
+			System.out.println("ry "+ry);
+			
+			
+			//if it goes up, make sure it goes up
+			if(isup) {
+				if(ry<1) {
+					ry*=-1;
+				}
+			}
+			//
+			
+			
+			
+			//if it is going down, make sure it goes down
+			else {
+				if(ry>1) {
+					ry*=-1;
+				}
+			}
+			//
+			
+		 finaly = lasty + ry;
+		 
+		
+		 
+		 
+		 
+		 
 	}
-	 public void draw() {
- 		pro.rect(x, y, cc.thin, cc.tall);
- 	
+
+	public void draw() {
+		// Make a random int between pro.ranges and pro.rangeb and get the previous
+		// lines y
+		
+		
+		pro.rect(x, finaly, pro.thin, pro.tall);
+
+	}
 }
-}
+
+//Make a random number between chains and chainb-
+//and have the line go a random number between ranges and rangeb relative to the previous-
+//lines y in a direction for the amount -
+//that the random number betweem chainb/s was
+
+//Then change direction for chains/b amount of times and loop this whole thing again
